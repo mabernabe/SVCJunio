7e6d3b4730dbacc18d616cd8a55d5390e0fda5b4
miguel <ma.bernabecruz@gmail.com>
2017-06-28 15:59:16 +0200

    C21, C21Test, C22, C22 Test

---
+++ src/test/java/C22Test.java
@@ -0,0 +1,23 @@
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C22Test {
+	
+	 private C22 c22;
+	 
+	 @Before
+	 public void before() {
+		 c22 = new C22();
+	 }
+
+	 @Test
+	 public void m1Test() {
+		 assertEquals("mA", this.c22.mA());
+	 }
+
+
+}
